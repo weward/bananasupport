08fdf829ff3d196b61ce165c98a148ca26dbbebb
weward <dev.weward@gmail.com>
2023-01-25 18:51:15 +0800

    feat: Added seeders for admin and users

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'info@codingmonkeys.nl',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+    }
+}
